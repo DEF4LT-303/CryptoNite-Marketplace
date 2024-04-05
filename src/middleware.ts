6bf4c138dec0074229ce0d176452105129bad990
DEF4LT-303 <ryanrafi72@gmail.com>
2024-04-05 23:15:52 +0600

    images folder fixed

--- src/middleware.ts
+++ src/middleware.ts
@@ -33,5 +33,5 @@ export default auth((req) => {
 
 // Optionally, don't invoke Middleware on some paths
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ['/((?!api|_next/static|_next/image|images|nav|favicon.ico).*)'],
 }
