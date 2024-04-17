40fcf7e18370be842e2b1055717b5bb240f1d02f
DEF4LT-303 <ryanrafi72@gmail.com>
2024-04-17 14:10:33 +0600

    dynamic order page

--- src/app/(protected)/(settings)/layout.tsx
+++ src/app/(protected)/(settings)/layout.tsx
@@ -39,9 +39,9 @@ export default function SettingsLayout({ children }: SettingsLayoutProps) {
   return (
     <>
       <MaxWidthWrapper>
-        <div className="space-y-6 p-10 pb-16">
+        <div className="space-y-6 p-1 my-5 mx-2 pb-16 sm:p-10">
           <div className="space-y-0.5">
-            <div className="flex flex-col sm:flex-row justify-between ">
+            <div className="flex flex-col sm:flex-row justify-between">
               <div>
                 <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
                 <p className="text-muted-foreground">
@@ -68,7 +68,7 @@ export default function SettingsLayout({ children }: SettingsLayoutProps) {
             </div>
           </div>
           <Separator className="my-6" />
-          <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+          <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0 ">
             <aside className="-mx-4 lg:w-1/5">
               <SidebarNav items={sidebarNavItems} />
             </aside>
