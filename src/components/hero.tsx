5fd3b44feb47e5c05e66a235bf44764dc6429e97
DEF4LT-303 <ryanrafi72@gmail.com>
2024-07-04 13:15:54 +0600

    globe added

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,11 +2,12 @@ import Link from "next/link";
 
 import MaxWidthWrapper from "./max-width-wrapper";
 import { Button, buttonVariants } from "./ui/button";
+import { GridGlobe } from "./ui/GridGlobe";
 
 const HeroSection = () => {
   return (
-    <MaxWidthWrapper>
-      <div className="bg-background">
+    <div className="bg-background relative overflow-hidden">
+      <MaxWidthWrapper>
         <div className="px-6 py-16 mx-auto duration-1000 animate-in animate fade-in-5 slide-in-from-right-2.5">
           <div className="items-center lg:flex">
             <div className="w-full lg:w-1/2">
@@ -31,16 +32,17 @@ const HeroSection = () => {
             </div>
 
             <div className="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
-              <img
+              {/* <img
                 className="w-full h-full lg:max-w-3xl"
                 src="https://merakiui.com/images/components/Catalogue-pana.svg"
                 alt="Catalogue-pana.svg"
-              />
+              /> */}
+              <GridGlobe />
             </div>
           </div>
         </div>
-      </div>
-    </MaxWidthWrapper>
+      </MaxWidthWrapper>
+    </div>
   );
 };
 
